9acee6dc6fd7212f216c0af1e93fe5446e41761c
Anthony Ubillús Avendaño <aua5@mscloud.ua.es>
2025-03-27 19:13:43 +0100

    soluciono el error de cors

--- index.js
+++ index.js
@@ -10,26 +10,26 @@ mongoose.connect('mongodb://localhost:27017/bck-ua');
 
 
 
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Origin not allowed by CORS'));
-        }
-    }
-}
-
-
 let app = express();
-app.use(logger('dev')); 
+
+
+app.use(
+    cors({
+        origin: "http://localhost:3000", // Permite solo tu app de React
+        methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+        allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos
+        credentials: true, // Si usas cookies o autenticación con tokens
+    })
+);
+
+
+app.use(logger('dev'));
 app.use(express.json({ limit: '25mb' }));
 
 app.use(express.urlencoded({ limit: '25mb' }));
 app.use('/auth', auth);
 
-app.use(cors());
-app.options('*', cors(corsOptions));
+
 
 
 app.listen(8080);
