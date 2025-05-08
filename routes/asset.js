a2bb427fbb931d6179e41398ecf4c41af32f6d9f
Anthony Ubillús Avendaño <aua5@mscloud.ua.es>
2025-05-08 12:51:11 +0200

    creo mas servicios para los assets

--- routes/asset.js
+++ routes/asset.js
@@ -47,6 +47,36 @@ router.get('/populares', (req, res) => {
     }
 });
 
+
+router.put('/:id', (req, res) => {
+
+    Asset.findByIdAndUpdate(req.params['id'], {
+        $set: {
+            nombre: req.body.nombre,
+            archivo: pathFoto,
+            descripcion: req.body.descripcion,
+            categorias: req.body.categorias,
+            tipo: req.body.tipo,
+            fecha_alta: new Date(),
+            autor: new mongoose.Types.ObjectId(req.body.autor)
+
+
+        }
+    }, {
+        new: true
+    }).then(x => {
+        res.status(200).send({
+            resultado: x
+        });
+
+    }).catch(() => {
+        res.status(500).send({
+            error: "No se ha encontrado al equipo"
+        });
+    });
+});
+
+
 router.get('/filtro/:categoria', (req, res) => {
 
     try {
@@ -56,7 +86,7 @@ router.get('/filtro/:categoria', (req, res) => {
         }).catch(err => {
             console.log(err);
             res.status(500).send({
-                error: 'No se han podido obtener los partidos'
+                error: 'No se han podido obtener los datos'
             });
         });;
 
@@ -65,5 +95,29 @@ router.get('/filtro/:categoria', (req, res) => {
         res.status(500).send({ error: 'Algo ha ido mal obteniendo los datos' });
     }
 });
+
+router.get('/:id', (req, res) => {
+    Asset.findById(req.params['id']).then(x => {
+        res.status(200).send({ resultado: x })
+
+    }).catch(err => {
+        res.status(500).send({
+            error: "No se ha podido encontrar los datos"
+        });
+    });
+});
+
+router.get('/:nombre', (req, res) => {
+    Asset.find({ categorias: req.params['nombre'] }).exec().then(x => {
+        res.send({ resultado: x });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).send({
+            error: 'No se han podido obtener los datos'
+        });
+    });;
+});
+
+
 
 module.exports = router;
