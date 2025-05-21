ab5441a614edd48ec7ce79d32e0f5533e8f1dcf4
Anthony Ubillús Avendaño <aua5@mscloud.ua.es>
2025-05-22 00:17:30 +0200

    modifco la ruta de los archivos

--- routes/asset.js
+++ routes/asset.js
@@ -45,11 +45,11 @@ function auth(req, res, next) {
 router.post('/subir', auth, async (req, res) => {
 
 
-    let pathFoto = `http://${req.hostname}:8080/assets/`;
+    let pathFoto = `http://${req.hostname}/assets/`;
 
     pathFoto += upload.storage(req.body.asset, 'assets');
 
-    let pathFotoPort = `http://${req.hostname}:8080/portadas/${upload.storage(req.body.portada, 'portadas')}`;
+    let pathFotoPort = `http://${req.hostname}/portadas/${upload.storage(req.body.portada, 'portadas')}`;
     console.log(req.user.id)
 
     let newAsset = new Asset({
